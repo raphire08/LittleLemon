a2ee53b645323651c485f24e9e5f18b9f76b75d0
Sumeet Makhija <sumeet.makhija.hp@gmail.com>
2023-11-01 20:12:56 +0530

    Initial Commit

---
+++ LittleLemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemon
+//
+//  Created by Admin on 01/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Admin on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
